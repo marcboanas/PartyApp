03788b3c0402d1586056d20baa13fa889e44ede8
Marc Boanas <marcboanas1@marcs-imac.home>
2018-03-27 11:30:40 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
+import { ApiService, HeaderComponent, SharedModule, UserService } from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    AuthModule,
+    BrowserModule,
+    HomeModule,
+    rootRouting,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/shared-module.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { ListErrorsComponent } from './list-errors.component';
+import { ShowAuthedDirective } from './show-authed.directive';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule
+  ],
+  declarations: [
+    ListErrorsComponent,
+    ShowAuthedDirective
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    HttpModule,
+    ListErrorsComponent,
+    ReactiveFormsModule,
+    RouterModule,
+    ShowAuthedDirective
+  ]
+})
+export class SharedModule {}
